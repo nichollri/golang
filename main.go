3c3b48d1a45835ed3e2f0812d39af8726d7777a4
EC2 Default User <ec2-user@ip-10-160-15-66.us-west-2.compute.internal>
2021-03-04 18:22:18 +0000

    added basic sleep cmd

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
+import "os/exec"
+import "log"
 
 func main() {
+        cmd := exec.Command("sleep", "5")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
 	fmt.Println("Hi there!")
 }
